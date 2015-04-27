ccf2f6fcb547a154d6d121897388df636357506b
= <=>
2015-04-26 23:57:51 -0700

    redirect

--- hello.py
+++ hello.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python
-from flask import Flask,request
+from flask import Flask,request,render_template,url_for,redirect
 app = Flask(__name__)
 
 
-@app.route('/login' , methods = ['GET'])
+@app.route('/login' , methods = ['GET', 'POST'])
 def login():
-    if request.values:
-        return 'username is %s' % request.values['username']
+    error = None
+    if request.method == 'POST':
+        if passwordcheck(
+             request.form.get('username'),
+             request.form.get('password')
+         ):
+            return redirect(url_for('welcome', username=request.form.get('username')))
+        else:
+            error = "Incorrect username or password"
+    return render_template('login.html', error=error)
+
+
+@app.route('/welcome/<username>')
+def welcome(username):
+    return render_template('welcome.html', username=username)
+
+
+def passwordcheck(username, password):
+    if username == password:
+        return True
     else:
-         return '<form method = "get" action="/login"><input type="text"  name ="username"/><p><button type="submit">Submit</button></form>'
-
-
+        return False
 if __name__ == '__main__':
     app.debug = True
     app.run()
